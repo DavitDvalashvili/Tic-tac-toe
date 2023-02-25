9399bf41adc8f0b616c9c2b23bc8d0be4b531e09
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-02-26 00:09:32 +0400

    finish home page

---
+++ script.js
@@ -0,0 +1,10 @@
+const chooseX = document.querySelector("#button-x");
+const chooseY = document.querySelector("#button-o");
+const buttons = document.querySelector(".buttons").childNodes;
+
+
+
+
+
+
+
