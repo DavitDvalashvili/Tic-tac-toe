f691682c10389b4c9407a3af6217afc9244bd794
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-03-03 16:31:34 +0400

    add several more functions

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const home = document.querySelector("#home");
 const board = document.querySelector("#board");
 const icon = document.querySelectorAll(".xo");
 const changeTurn = document.querySelectorAll(".turn img")[0];
+const scoreX = document.querySelector("#x-score-num");
+const scoreO = document.querySelector("#o-score-num");
+const scoreTie = document.querySelector("#ties-num");
+const resultBoard = document.querySelector("#result");
+const resultText = document.querySelector(".result-text")
+const oTakes = document.querySelector(".o-takes")
+const xTakes = document.querySelector(".x-takes")
+const tied = document.querySelector(".tied");
+const quitButton = document.querySelector("#quit");
+const nextButton = document.querySelector("#next");
 
-
+let xScore = 0;
+let OScore = 0;
+let tieScore = 0;
 let player1 = "o";
 let player2 ="x";
 let mode = "cpu";
@@ -72,22 +84,91 @@ function placeIcons() {
                 turn = "o"
                 target.parentElement.classList.remove("hoverX");
                 target.parentElement.classList.add("hoverO");
+                newArr.push(turn)
             } else if (turn == "o" && target.classList.contains("addX") != true && target.classList.contains("addO") != true) {
                 target.classList.add("addO")
                 changeTurn.src = "./assets/silver-x.svg"
                 turn = "x"
                 target.parentElement.classList.remove("hoverO");
                 target.parentElement.classList.add("hoverX");
+                newArr.push(turn);
             }
-            newArr.push(turn);
+            checkWin();
         })
-    });
+    })
 }
 
 
+//checks hwo win x, o or it was tie;
+function checkWin() {
+    if(checkWinX()) {
+        winX();
+    } else if (checkWinO()) {
+        winO();
+    } else if(newArr.length == 9) {
+        makeTie();
+    }
+}
 
 
+//checks if X win
+function checkWinX() {
+   return winPattern.some(combination => {
+        return combination.every(index => {
+            return icon[index].classList.contains("addX");
+            
+        })
+    })
+}
 
+//checks if  O win
+function checkWinO() {
+    return winPattern.some(combination => {
+         return combination.every(index => {
+             return icon[index].classList.contains("addO");
+         })
+     })
+}
 
+//displays result on the screen when x wins
+function winX() {
+    xScore++;
+    scoreX.textContent = xScore;
+    resultBoard.style.display = "flex";
+    oTakes.style.display = "none"
+    tied.style.display = "none";
+    if(player1 == "x") {
+        resultText.textContent = "PLAYER 1 WINS!"
+    } else {
+        resultText.textContent = "PLAYER 2 WINS!"
+    }
+    
+}
 
+//displays result on the screen when 0 wins
+function winO() {
+    OScore++;
+    scoreO.textContent = OScore;
+    resultBoard.style.display = "flex";
+    xTakes.style.display = "none"
+    tied.style.display = "none";
+    if(player1 == "o") {
+        resultText.textContent = "PLAYER 1 WINS!"
+    } else {
+        resultText.textContent = "PLAYER 2 WINS!"
+    }
+}
+
+//displays result on the screen when ties
+function makeTie() {
+    tieScore++;
+    scoreTie.textContent= tieScore;
+    resultBoard.style.display = "flex";
+    oTakes.style.display = "none"
+    xTakes.style.display = "none"
+}
+
+function quitGame() {
+    
+}
 
