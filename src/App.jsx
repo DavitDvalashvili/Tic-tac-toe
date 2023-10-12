7c2fd59141129cdc645cf90274593ca8cdd463e7
davit dvalashvili <davitdvalashvili1996@gmail.com>
2023-10-12 07:51:51 +0400

    wip: work in progress

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import PvPGame from "./components/PersonVsPerson/PersonVsPersonGame";
+import PvCGame from "./components/PersonVsCpu/PersonVsCpuGame";
+import NewGameMenu from "./components/UI/NewGameMenu";
+import "./App.css";
+
+function App() {
+  const [showMenu, setShowMenu] = useState(true);
+  const [pvpGameStart, setPvpGameStart] = useState(false);
+  const [pvcGameStart, setPvcGameStart] = useState(false);
+  const [playerOne, setPlayerOne] = useState("");
+  const [playerTwo, setPlayerTwo] = useState("");
+  const [playerCpu, setPlayerCpu] = useState("");
+
+  function refreshPage() {
+    window.location.reload(true);
+  }
+
+  function setPlayerOneIcon(icon) {
+    setPlayerOne(icon);
+  }
+
+  function setPlayerTwoIcon(icon) {
+    setPlayerTwo(icon);
+  }
+
+  function setPlayerCpuIcon(icon) {
+    setPlayerCpu(icon);
+  }
+
+  function displayMenuHandler() {
+    setShowMenu(!showMenu);
+  }
+
+  function startPvpGame() {
+    setPvpGameStart(!pvpGameStart);
+  }
+
+  function startPvcGame() {
+    setPvcGameStart(!pvcGameStart);
+  }
+
+  return (
+    <main className="centered-container">
+      {showMenu && (
+        <NewGameMenu
+          onDisplayMenuHandler={displayMenuHandler}
+          onSetPlayerOneIcon={setPlayerOneIcon}
+          onSetPlayerTwoIcon={setPlayerTwoIcon}
+          onSetPlayerCpuIcon={setPlayerCpuIcon}
+          onStartPvpGame={startPvpGame}
+          onStartPvcGame={startPvcGame}
+          playerOne={playerOne}
+          playerTwo={playerTwo}
+          playerCpu={playerCpu}
+        />
+      )}
+      {!showMenu && pvpGameStart && !pvcGameStart && (
+        <PvPGame
+          onShowMenu={setShowMenu}
+          onRefreshPage={refreshPage}
+          playerOne={playerOne}
+          playerTwo={playerTwo}
+        />
+      )}
+      {!showMenu && pvcGameStart && !pvpGameStart && (
+        <PvCGame
+          onShowMenu={setShowMenu}
+          onRefreshPage={refreshPage}
+          playerOne={playerOne}
+          playerCpu={playerCpu}
+        />
+      )}
+    </main>
+  );
+}
+
+export default App;
